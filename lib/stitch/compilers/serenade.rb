c70a57da57cabbd296ffe81bed1f916093fd2bde
Patrik Sundberg <patrik.sundberg@gmail.com>
2012-02-14 19:33:30 +0000

    make sure template is properly escaped.

--- lib/stitch/compilers/serenade.rb
+++ lib/stitch/compilers/serenade.rb
@@ -19,7 +19,7 @@ module Stitch
       # always remove .serenade ending
       serenade_path.gsub!(/\.serenade$/, "")
 
-      %{var viewTemplate = '#{content}';
+      %{var viewTemplate = #{content.inspect};
         var view = Serenade.view('#{serenade_path}', viewTemplate);
         module.exports = view;
       }
